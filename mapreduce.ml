aa368a674cb7debc6c1c2608f89cccd0e1e492a4
ty-kay <98359964+ty-kay@users.noreply.github.com>
2022-09-25 15:31:15 -0400

    Update mapreduce.ml

--- mapreduce.ml
+++ mapreduce.ml
@@ -2,20 +2,8 @@
 
 Name: Ty Kay
 Netid: tkay
-Minutes Spent on Problem 1.1:
-Minutes Spent on Problem 1.2:
-
-(You aren't in any way graded on the number of minutes spent; 
- we are just trying to calibrate for future versions of the class)
-
-Comments/Problems/Thoughts on this part of the assignment:
 
 *)
-
-(* This part of the assignment uses the following functions 
- * If you forget how they work, look them up:
- * http://caml.inria.fr/pub/docs/manual-ocaml/libref/List.html
- *)
 
 let map : ('a -> 'b) -> 'a list -> 'b list = List.map
 
@@ -32,65 +20,27 @@ let rec reduce (f:'a -> 'b -> 'b) (u:'b) (xs:'a list) : 'b =
     | [] -> u
     | hd::tl -> f hd (reduce f u tl) 
 
-(***********************************************)
-(******            1.1 WARM UP            ******)
-(***********************************************)
-
-(* Solve each problem in this part using map, reduce, foldl, foldr or
- * filter.  Map, filter, reduce, foldl, foldr are an example of a
- * *combinator library*: a library of higher-order functions used to
- * solve problems in a particular domain.  In this case, the domain is
- * list-processing.  However, there are countless other useful
- * combinator libraries.  The goal is to get used to thinking about how
- * to decompose complex functions in to smaller, simpler, orthogonal
- * functional components.  The smaller, simpler functional components
- * can be constructed directly using the combinator library.
- * 
- * Note: foldl is slightly more efficient than foldr because it is tail
- * recursive.  (We will explain what that means later in the course.)
- * Hence, solutions that use foldl are typically superior to solutions
- * that use foldr, all other things being equal.  Thus you should try
- * to use foldl where you can instead of foldr (but take care to retain
- * good style -- a horribly convoluted, incomprehensible function that
- * uses foldl is worse than an elegant one that uses foldr).
- * 
- * In these problems, you are not allowed to use the "rec" keyword in
- * your solution.  A solution, even a working one, that uses explicit
- * recursion via "rec" will receive little to no credit.  You may write
- * useful auxiliary functions; they just may not be recursive.  
- * (The goal of this part of the assignment is not to simulate
- * everyday programming.  It is to get you think about writing programs
- * in a different style from what you are used to.  We are trying to
- * enlarge your programming toolkit.)
- * 
- * You are also not allowed to use other functions from a standard or
- * external library such as sort, concat or flatten, nor the built-in
- * list operator @.  (You are allowed to recode those functions
- * yourself using map, filter, fold if you find it necessary.)
- * 
- *)
-
 (*>* Problem 1.1.a *>*)
 
 (*  negate_all : Flips the sign of each element in a list *)
+
 let negate (x: int) : int =
   0 - x
+  
 let negate_all (nums:int list) : int list =
   map negate nums
 
-
-(* Unit test example.  Uncomment after writing negate_all *)
 let _ = assert ((negate_all [1; -2; 0]) = [-1; 2; 0])
-
 
 (*>* Problem 1.1.b *>*)
 
 (*  sum_rows : Takes a list of int lists (call an internal list a "row").
  *             Returns a one-dimensional list of ints, each int equal to the
- *             sum of the corresponding row in the input.
- *   Example : sum_rows [[1;2]; [3;4]] = [3; 7] *)
- let sum (row:int list) : int =
+ *             sum of the corresponding row in the input. *)
+ 
+let sum (row:int list) : int =
   foldl (+) 0 row
+  
 let sum_rows (rows:int list list) : int list =
   map sum rows
 
@@ -98,8 +48,7 @@ let _ = assert (sum_rows [[1;2]; [3;4]] = [3; 7])
 
 (*>* Problem 1.1.c *>*)
 
-(*  num_occurs : Returns the number of times a given number appears in a list.
- *     Example : num_occurs 4 [1;3;4;5;4] = 2 *)
+(*  num_occurs : Returns the number of times a given number appears in a list. *)
 
 let num_occurs (n:int) (nums:int list) : int =
   let y = (filter (fun x -> x = n) nums) in
@@ -110,27 +59,12 @@ let _ = assert (num_occurs 4 [1;3;4;5;4] = 2)
 
 (*>* Problem 1.1.d *>*)
 
-(*  super_sum : Sums all of the numbers in a list of int lists
- *    Example : super_sum [[1;2;3];[];[5]] = 11 *)
+(*  super_sum : Sums all of the numbers in a list of int lists *) 
+
 let super_sum (nlists:int list list) : int =
   sum (sum_rows nlists)
 
 let _ = assert (super_sum [[1;2;3];[];[5]] = 11)
-
-(****************************************************)
-(**********       1.2 A Bigger Challenge   **********)
-(****************************************************)
-
-(* Note: some of these questions may be challenging. Don't neglect
- * Part 2 of this assignment because you are working on these problems.
- * 
- * Again, these problems should be solved using a higher-order function
- * -- we intend that you use one or more folds, though map and filter
- * are acceptable if you find them useful -- and very little to no
- * credit will be given for doing them with explicit recursion. 
- *
- *)
-
 
 (*>* Problem 1.2.a *>*)
 
@@ -183,6 +117,7 @@ let pre (x: 'a) (xs: 'a list list) : 'a list list =
   | [] -> [[x]]
   | hd :: tl -> 
     [x] :: (map (fun z -> x :: z) xs)
+    
 let prefixes (xs: 'a list) : 'a list list =
   reduce pre [] xs
 
@@ -191,13 +126,9 @@ let _ = assert (prefixes [1;2;3;4] = [[1]; [1;2]; [1;2;3]; [1;2;3;4]])
 (*>* Problem 1.2.c *>*)
 (* flatten : write a function that flattens a list of lists into a single
  * list with all of the elements in the order they appeared in the original 
- * lists.
+ * lists
  *
- * flatten [[1;2;3]; []; [0]; [4;5]] = [1;2;3;0;4;5] 
- *
- * In the last assignment you wrote this function with recursion. Now, 
- * do it without explicit recursion.
- *)
+ * flatten [[1;2;3]; []; [0]; [4;5]] = [1;2;3;0;4;5] *)
 
 let cons (x: 'a) (y: 'a list) : 'a list =
   x :: y 
