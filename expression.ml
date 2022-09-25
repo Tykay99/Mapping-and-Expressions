b0114abececb614b25f66ae0b15827ecb2eddcae
ty-kay <98359964+ty-kay@users.noreply.github.com>
2022-09-25 15:35:51 -0400

    Add files via upload

---
+++ expression.ml
@@ -0,0 +1,141 @@
+(* 
+
+Name: Ty Kay
+Email: tkay@princeton.edu
+Minutes Spent on the for-credit parts of Problem 2:
+
+(You aren't in any way graded on the number of minutes spent; 
+ we are just trying to calibrate for future versions of the class)
+
+Comments/Problems/Thoughts on this part of the assignment:
+
+*)
+
+open Ast 
+open ExpressionLibrary 
+
+(* TIPS FOR PROBLEM 2:
+ * 1. Read the writeup.
+ * 2. Use the type definitions in the ast.ml as a reference. But don't worry 
+ *    about expressionLibrary.ml
+ * 3. Test!  (Use "assert" where appropriate.)
+ *)
+
+
+(*>* Problem 2.1 *>*)
+
+(* evaluate : evaluates an expression for a particular value of x. 
+ *  Example : evaluate (parse "x*x + 3") 2.0 = 7.0 *)
+
+let rec evaluate (e:expression) (x:float) : float =
+  match e with
+  | Num n -> n
+  | Var -> x
+  | Binop (b,e1,e2) -> match b with
+    | Add -> evaluate(e1)(x) +. evaluate(e2)(x)
+    | Sub -> evaluate(e1)(x) -. evaluate(e2)(x)
+    | Mul -> evaluate(e1)(x) *. evaluate(e2)(x)
+
+(*>* Problem 2.2 *>*)
+
+let rec derivative (e:expression) : expression =
+  match e with
+  | Num n -> Num 0.0
+  | Var -> Num 1.0
+  | Binop (b,e1,e2) -> match b with
+    | Mul -> Binop(Add,(Binop(Mul,derivative(e1),e2)),
+    (Binop(Mul,derivative(e2),e1)))
+    | _ -> Binop(b,derivative(e1),derivative(e2))
+
+(* A helpful function for testing. See the writeup. *)
+let checkexp strs xval=
+  print_string ("Checking expression: " ^ strs^"\n");
+  let parsed = parse strs in (
+	print_string "Result of evaluation: ";
+	print_float  (evaluate parsed xval);
+	print_endline " ";
+	print_string "Result of derivative: ";
+	print_endline " ";
+	print_string (to_string (derivative parsed));
+	print_endline " ")
+
+
+(*>* Problem 2.3 *>*)
+
+(* See writeup for instructions. *)
+
+let rec find_zero (e:expression) (g:float) (epsilon:float) (lim:int)
+    : float option =
+  if lim < 0 then None
+  else let temp = evaluate e g in
+  if temp < epsilon && (-.temp < epsilon) then Some g
+  else let update = g -. temp /. evaluate (derivative(e)) g in
+  find_zero e update epsilon (lim - 1)
+
+(*>* Problem 2.4 *>*)
+
+(* See writeup for instructions. *)
+(* let zero(e:expression) : option =
+  let der = derivative(e) in
+  match der with
+  | Num 0.0 -> None
+  | Num x -> Some
+  | _ -> Some *)
+
+(* let rec moveToFront (e : expression) (n: int): expression = *)
+
+let rec simplify (e : expression) : expression =
+  match e with
+  | Num n -> e
+  | Var -> e
+  | Binop (b,e1,e2) -> match b with
+    | Mul -> Binop(Add,(Binop(Mul,derivative(e1),e2)),
+    (Binop(Mul,derivative(e2),e1)))
+    | _ -> Binop(b,simplify(e1),simplify(e2))
+
+let rec find_zero_exact (e:expression) : expression option =
+  failwith "Not implemented" 
+
+(* For extra fun (but not extra credit),
+  implement find_zero_exact2 that solves degree-two expressions.
+  This is almost as easy as solving degree-one expressions,
+  if you use the quadratic formula.  Almost as easy, assuming
+  you've already done the work to normalize polynomials into an
+  easily recognizable form. *)
+
+
+(* For extra fun (but not extra credit), 
+
+ Consider this function,
+  let evaluate2 (e: expression) : float -> float =
+     let e' = derivative e in
+     fun x -> (evaluate e x, evaluate e' x)
+
+ Such a function can be used in Newton's method.
+ But if the expression e is large, e' can be exponentially larger,
+ because of the chain rule for multiplication, so
+ evaluate e' x  can be slow.
+
+ One solution is called "forward mode automatic differentiation",
+ which has become an important algorithm (since 2017 or so) in
+ deep learning.  You can read about it in section 3.1 of
+ this paper:  http://jmlr.org/papers/volume18/17-468/17-468.pdf
+ "Automatic Differentiation in Machine Learning: A Survey"
+ (and pay particular attention to Table 2 for a worked example).
+
+ So, the challenge (which is actually not very difficult) is,
+ write this function
+
+  let evaluate2 (e: expression) (x: float) : float * float = ...
+
+ that computes both e(x) and the first derivative e'(x),
+ without ever calculating (derivative e).  Like evaluate,
+ do it by case analysis on the syntax-tree of e. *)
+	   
+(* Q.  Why do it, if no extra credit?
+   A.  Because (and only if) it's fun.  
+   A.  Because the main reason you're working so hard at Princeton
+       is to learn things, not just to get grades.
+   A.  Any well educated computer scientist graduating after 2019
+     ought to know something about deep learning . . .
+ *)
